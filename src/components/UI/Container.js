af4041cafad59c15b72985d8c0a843e02fcae4be
Clearsu <clearwater1219@gmail.com>
2023-08-17 19:55:28 +0900

    chore: initialize

--- src/components/UI/Container.js
+++
@@ -1,9 +0,0 @@
-import "./Container.css";
-
-const Container = (props) => {
-  const classes = "container " + props.classes;
-
-  return <div className={classes}>{props.children}</div>;
-};
-
-export default Container;
--- src/components/UI/TopNav.js
+++
@@ -1,9 +0,0 @@
-import "./TopNav.css";
-
-const TopNav = (props) => {
-  const classes = "top-navigation-bar " + props.classes;
-
-  return <div className={classes}>{props.children}</div>;
-};
-
-export default TopNav;
