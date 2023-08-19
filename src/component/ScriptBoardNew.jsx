3b2ed734680794e06e9acece355409dc9dca61b2
Clearsu <clearwater1219@gmail.com>
2023-08-20 00:48:14 +0900

    feat: Container max-width

--- src/component/ScriptBoardNew.jsx
+++ src/component/ScriptBoardNew.jsx
@@ -4,6 +4,7 @@ import "./ScriptBoardNew.css";
 import GoBackButton from "./Util/Button/GoBackButton";
 
 import Header from "./Util/Header";
+import Container from "./Util/Container";
 
 const ScriptBoardNew = () => {
   const [inputTitle, setInputTitle] = useState("");
@@ -37,25 +38,27 @@ const ScriptBoardNew = () => {
 
   return (
     <form onSubmit={submitHandler}>
-      <Header
-        pageTitle="스크립트 게시판 글쓰기"
-        leftButton={<GoBackButton path="/boards/script-boards" />}
-      ></Header>
-      <div className={`form-control ${isTitleValid ? "" : "invalid"}`}>
-        <input
-          placeholder="제목을 입력하세요."
-          onChange={titleChangeHandler}
-        ></input>
-      </div>
-      <button>파일 첨부</button>
-      <span>test.sh</span>
-      <div className={`form-control ${isDetailValid ? "" : "invalid"}`}>
-        <textarea
-          placeholder="내용을 입력하세요."
-          onChange={detailChangeHandler}
-        ></textarea>
-      </div>
-      <button type="submit">등록</button>
+      <Container>
+        <Header
+          pageTitle="스크립트 게시판 글쓰기"
+          leftButton={<GoBackButton path="/boards/script-boards" />}
+        ></Header>
+        <div className={`form-control ${isTitleValid ? "" : "invalid"}`}>
+          <input
+            placeholder="제목을 입력하세요."
+            onChange={titleChangeHandler}
+          ></input>
+        </div>
+        <button>파일 첨부</button>
+        <span>test.sh</span>
+        <div className={`form-control ${isDetailValid ? "" : "invalid"}`}>
+          <textarea
+            placeholder="내용을 입력하세요."
+            onChange={detailChangeHandler}
+          ></textarea>
+        </div>
+        <button type="submit">등록</button>
+      </Container>
     </form>
   );
 };
