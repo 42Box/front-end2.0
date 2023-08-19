4df56a7b9eb7e6f5b5195869e5e221e088f5e45a
Clearsu <clearwater1219@gmail.com>
2023-08-19 12:11:04 +0900

    remove(component): remove Card

--- src/component/Util/Card.js
+++
@@ -1,9 +0,0 @@
-import "./Card";
-
-const Card = (props) => {
-  const classes = "simple-card" + props.classes;
-
-  return <div className={classes}>{props.children}</div>;
-};
-
-export default Card;
