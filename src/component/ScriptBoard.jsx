cca90cd60b84480c5224d51756bf3b878b873958
Jincheol Park <67998022+Clearsu@users.noreply.github.com>
2023-08-20 18:23:06 +0900

    feat(Comment): comment textarea box (#17)
    * feat: Comment input box
    
    * feat(CommentNew): fix css
    
    * feat: Container max-width

--- src/component/ScriptBoard.jsx
+++ src/component/ScriptBoard.jsx
@@ -1,20 +1,21 @@
 import Header from "./Util/Header";
 import RefreshButton from "./Util/Button/RefreshButton";
 import WriteButton from "./Util/Button/WriteButton";
+import Container from "./Util/Container";
 
 import TextPreviewList from "./TextPreviewList";
 import dummyPosts from "../dummyPosts";
 
 const ScriptBoard = () => {
   return (
-    <div>
+    <Container>
       <Header
         pageTitle="스크립트 게시판"
         leftButton={<RefreshButton />}
         rightButton={<WriteButton path="/boards/script-boards/new" />}
       ></Header>
       <TextPreviewList posts={dummyPosts} />
-    </div>
+    </Container>
   );
 };
 
