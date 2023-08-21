e9909f1f58a6e204dafc46cdc829d3c42162651a
Jincheol Park <67998022+Clearsu@users.noreply.github.com>
2023-08-21 14:50:46 +0900

    feat: RandingPage (#43)
    * chore: add gitignore
    
    * chore: add mandatory files
    
    * wip: ui prototype
    
    * chore: gitignore
    
    * chore: remove package-lock.json
    
    * chore: change name
    
    * chore: update package.json
    
    * chore: add package-lock.json
    
    * Update README.md
    
    * rename: rename files
    
    * rename: roll-back
    
    * build: add Chakra, MUI
    
    * chore: initialize
    
    * chore: change page title
    
    * build: add icons, router-dom
    
    * wip: Header, Buttons
    
    * wip: TextPreview component
    
    * build: remove MUI, Chakra
    
    * feat(Date): Date component
    
    * chore: dummy data for test
    
    * fix(Header): fix className
    
    * feat(ScriptBoard): ScriptBoard kkangtong
    
    * feat: ScriptBoardNew component
    
    * feat(component): Container
    
    * feat(component): CommentList
    
    * remove(component): remove Card
    
    * feat(App): add temporary Route for detail view
    
    * feat(component): ScriptBoardContent
    
    * fix(component): fix Header
    
    * chore: rename files
    
    * feat(ScriptBoard): click event for detail view (#13)
    
    * refactor(GoBackButton): change prop name
    
    * feat: link to content detail
    
    * refactor: change member name
    
    * feat(ScriptBoard): buttons
    
    * chore: add file for CICD
    
    * chore: mv deploy-main.yml to /.github/workflows/
    
    * feat(Comment): comment textarea box (#17)
    
    * feat: Comment input box
    
    * feat(CommentNew): fix css
    
    * feat: Container max-width
    
    * feat(ScriptBoardNew): add file input (#19)
    
    * fix(ScriptBoardNew): fix button props (#27)
    
    * feat(ScriptBoardContent): add Container component (#29)
    
    * test: test route (#34)
    
    * fix: use unusing var (#37)
    
    * feat: custom hook for api call (#39)
    
    * build: add axios
    
    * wip: wip
    
    * feat: apiHook
    
    * doc: remove documentation
    
    * refactor: remove unusing import
    
    * fix: RandingPage component close #15 (#40)
    
    * fix: RandingPage component close #15
    
    * fix: readme #15
    
    ---------
    
    Co-authored-by: jiyun <tomatoziyun@gmail.com>
    
    ---------
    
    Co-authored-by: jiyun <70049994+tomatozil@users.noreply.github.com>
    Co-authored-by: jiyun <tomatoziyun@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+import RandingPage from "./component/main/RandingPage";
 import ScriptBoard from "./component/ScriptBoard";
 import ScriptBoardSelect from "./component/ScriptBoardSelect";
 
@@ -7,10 +8,11 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<ScriptBoard />} />
+        <Route path="/" element={<RandingPage />} />
         <Route path="/boards/script-boards" element={<ScriptBoard />} />
         <Route
           path="/boards/script-boards/:id"
+          ㅌ
           element={<ScriptBoardSelect />}
         />
       </Routes>
