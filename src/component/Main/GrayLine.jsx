ee629fb27a6151f9c9e427706086a4364b27c70a
Clearsu <clearwater1219@gmail.com>
2023-08-25 22:09:38 +0900

    feat: main page design done

---
+++ src/component/Main/GrayLine.jsx
@@ -0,0 +1,20 @@
+const GrayLine = () => {
+  return (
+    <svg
+      width="704"
+      height="1"
+      viewBox="0 0 704 1"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M1 0.5H703"
+        stroke="#C7C7C7" // 직접 색상 값을 지정
+        strokeWidth="1"
+        strokeLinecap="round"
+      />
+    </svg>
+  );
+};
+
+export default GrayLine;
