d75ff62f3543c345a240ce3f03513229cb2ec5a2
Clearsu <clearwater1219@gmail.com>
2023-08-28 00:40:37 +0900

    refactor: devide component

---
+++ src/component/Main/MainTitle.jsx
@@ -0,0 +1,11 @@
+import { Text } from "@chakra-ui/react";
+
+const MainTitle = () => {
+  return (
+    <Text fontSize="30px" fontWeight="700" display="inline-flex">
+      42Box
+    </Text>
+  );
+};
+
+export default MainTitle;
