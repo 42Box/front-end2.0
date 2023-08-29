92f1365074e31060e3f2ecc2f534dcc2f997bcf7
jiyun <tomatoziyun@gmail.com>
2023-08-29 14:20:42 +0900

    feat: script download and reload

--- src/component/ScriptBoard/ScriptBoardContent.jsx
+++ src/component/ScriptBoard/ScriptBoardContent.jsx
@@ -1,21 +1,21 @@
-import "./ScriptBoardContent.css";
-
+import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 import Header from "../Util/Header";
 import Container from "../Util/Container";
-import { FaEllipsis } from "react-icons/fa6";
-import { BiSolidUpArrow, BiSolidRightArrow } from "react-icons/bi";
-//import { useParams } from "react-router-dom";
-
 import CommentNew from "../Comment/CommentNew";
 import CommentList from "../Comment/CommentList";
-
+import { FaEllipsis } from "react-icons/fa6";
+import { BiSolidUpArrow, BiSolidRightArrow } from "react-icons/bi";
+import "./ScriptBoardContent.css";
+import axios from "axios";
 import dummyComments from "../../dummyComments";
-import { useState } from "react";
 
 const ScriptBoardContent = () => {
+  const navigate = useNavigate();
   const [openPreview, setOpenPreview] = useState(false);
-  // const params = useParams();
-  // const { id } = params;
+  const params = useParams();
+  const { id } = params;
   // api/id 요청해서 없으면 오류메시지
 
   const scriptContent =
@@ -26,6 +26,28 @@ const ScriptBoardContent = () => {
     "Hooks are more restrictive than other functions. " +
     "You can only call Hooks at the top of your components (or other Hooks). " +
     "If you want to use useState in a condition or a loop, extract a new component and put it there.";
+
+  const downloadFile = async (path) => {
+    try {
+      const response = await axios.get(path, {
+        responseType: "blob", // Set the response type to blob
+      });
+
+      const url = window.URL.createObjectURL(new Blob([response.data]));
+      const a = document.createElement("a");
+      a.href = url;
+      a.download = path.substring(path.lastIndexOf("/") + 1); // 파일 이름 추출
+      document.body.appendChild(a);
+      a.click();
+      a.remove();
+
+      navigate("/script/content/" + id);
+      window.location.reload();
+    } catch (error) {
+      console.error("Error downloading file:", error);
+      // 추후 실패 모달
+    }
+  };
 
   return (
     <Container backgroundColor="#ffffff">
@@ -51,8 +73,16 @@ const ScriptBoardContent = () => {
         <div>{scriptContent}</div>
         <div>
           <div>
+            <button
+              onClick={() =>
+                downloadFile(
+                  "https://42box.kr/user_profile_image/cleanCache.sh",
+                )
+              }
+            >
+              다운로드
+            </button>
             <button>실행</button>
-            <button>다운로드</button>
           </div>
           <div>
             <button>좋아요</button>
