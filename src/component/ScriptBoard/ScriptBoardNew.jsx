f54e9a4ddda5eb1d31042470bf589a67e0969a7c
Clearsu <clearwater1219@gmail.com>
2023-08-31 14:05:38 +0900

    wip: wip

--- src/component/ScriptBoard/ScriptBoardNew.jsx
+++ src/component/ScriptBoard/ScriptBoardNew.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import useApi from "../../hook/useApi";
 
 import "./ScriptBoardNew.css";
 
@@ -6,6 +7,12 @@ import Header from "../Util/Header";
 import Container from "../Util/Container";
 
 const ScriptBoardNew = () => {
+  const postFormData = useApi("POST", "board-service/script-boards", formData: {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+
   const [inputTitle, setInputTitle] = useState("");
   const [inputDetail, setInputDetail] = useState("");
   const [isTitleValid, setIsTitleValid] = useState(true);
@@ -13,12 +20,18 @@ const ScriptBoardNew = () => {
   const [selectedFile, setSelectedFile] = useState(null);
 
   const titleChangeHandler = (event) => {
-    setIsTitleValid(event.target.value.trim().length > 2);
+    setIsTitleValid(
+      event.target.value.trim().length >= 10 &&
+        event.target.value.trim().length <= 50
+    );
     setInputTitle(event.target.value);
   };
 
   const detailChangeHandler = (event) => {
-    setIsDetailValid(event.target.value.trim().length > 2);
+    setIsDetailValid(
+      event.target.value.trim().length >= 10 &&
+        event.target.value.trim().length <= 2000
+    );
     setInputDetail(event.target.value);
   };
 
@@ -27,14 +40,34 @@ const ScriptBoardNew = () => {
     setSelectedFile(file);
   };
 
-  const submitHandler = (event) => {
-    event.preventDefault(); // block sending GET request
+  const submitHandler = async (event) => {
+    event.preventDefault();
     console.log(inputTitle);
     console.log(inputDetail);
     if (!isTitleValid || !isDetailValid || !selectedFile) {
       alert(
-        "⚠️글을 등록할 수 없습니다.⚠️\n파일이 첨부되었는지, 제목과 내용을 2자 이상 입력하였는지 확인해주세요.",
-      ); // 추후 디자인 적용
+        "⚠️글을 등록할 수 없습니다.⚠️\n파일이 첨부되었는지, 제목과 내용을 지정된 길이에 맞게 입력하였는지 확인해주세요."
+      );
+    } else {
+      const formData = new FormData();
+      formData.append("script-file", selectedFile);
+      formData.append(
+        "body",
+        JSON.stringify({
+          title: inputTitle,
+          content: inputDetail,
+          scriptName: "my-script",
+        })
+      );
+
+      postFormData(
+        () =>
+          console.log("Post successful"),
+          // Handle success response as needed
+        (error) =>
+          console.error("Error posting script:", error)
+          // Handle error response as needed
+      );
     }
   };
 
