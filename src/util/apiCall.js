2aec2a39331c4ab82d32cf308da38328ac059dac
Jincheol Park <67998022+Clearsu@users.noreply.github.com>
2023-08-31 16:37:06 +0900

    feat: ScriptBoradNew api (#143)

---
+++ src/util/apiCall.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://api.42box.site/",
+  withCredentials: true,
+});
+
+const apiCall = async (method, url, data) => {
+  try {
+    if (method === "GET") {
+      return await api.get(url, data);
+    }
+    if (method === "POST") {
+      return await api.post(url, data);
+    }
+    if (method === "DELETE") {
+      return await api.delete(url);
+    }
+    if (method === "PUT") {
+      return await api.put(url, data);
+    }
+    if (method === "OPTIONS") {
+      return await api.options(url);
+    }
+  } catch (error) {
+    throw error;
+  }
+};
+
+export default apiCall;
