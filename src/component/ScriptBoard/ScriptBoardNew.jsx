8379458d77940cd903938e42d943a395aabdcdc1
Jincheol Park <67998022+Clearsu@users.noreply.github.com>
2023-09-03 01:46:49 +0900

    feat: board new design (#203)

--- src/component/ScriptBoard/ScriptBoardNew.jsx
+++ src/component/ScriptBoard/ScriptBoardNew.jsx
@@ -1,7 +1,8 @@
-import "./ScriptBoardNew.css";
 import Header from "../Util/Header";
-import Container from "../Util/Container";
 import useScriptBoardNew from "../../hook/useScriptBoardNew";
+import BackGround from "../Util/BackGround";
+import { Input, Text, Textarea, Box, Button, Flex } from "@chakra-ui/react";
+import FileSelectButton from "../Util/Button/FileSelectButton";
 
 const ScriptBoardNew = () => {
   const {
@@ -27,10 +28,15 @@ const ScriptBoardNew = () => {
     setSelectedFile(file);
   };
 
-  const submitHandler = async (event) => {
-    event.preventDefault();
-    console.log(inputTitle);
-    console.log(inputDetail);
+  const selectFileHandler = () => {
+    const fileInput = document.getElementById("fileInput");
+    if (fileInput) {
+      fileInput.click();
+      fileInput.value = "";
+    }
+  };
+
+  const validateForm = () => {
     if (
       inputTitle.length < 10 ||
       inputTitle.length > 40 ||
@@ -38,35 +44,96 @@ const ScriptBoardNew = () => {
       inputDetail.length > 5000 ||
       !selectedFile
     ) {
+      return false;
+    }
+    return true;
+  };
+
+  const submitHandler = async (event) => {
+    event.preventDefault();
+    if (validateForm()) {
+      postFormData();
+    } else {
       alert(
         "⚠️글을 등록할 수 없습니다.⚠️\n파일이 첨부되었는지, 제목과 내용을 지정된 길이에 맞게 입력하였는지 확인해주세요."
       );
-    } else {
-      postFormData();
     }
   };
 
   return (
-    <form onSubmit={submitHandler}>
-      <Container>
-        <Header pageTitle="스크립트 게시판 글쓰기"></Header>
-        <div className={"form-control"}>
-          <input
-            placeholder="제목을 입력하세요."
-            onChange={titleChangeHandler}
-          ></input>
-        </div>
-        <input type="file" onChange={fileChangeHandler} />
-        {selectedFile && <span>{selectedFile.name}</span>}
-        <div className={"form-control"}>
-          <textarea
-            placeholder="내용을 입력하세요."
-            onChange={detailChangeHandler}
-          ></textarea>
-        </div>
-        <button type="submit">등록</button>
-      </Container>
-    </form>
+    <BackGround>
+      <Header pageTitle="스크립트"></Header>
+      <Box width="704px" alignSelf="center">
+        <form onSubmit={submitHandler}>
+          <Box paddingTop="36px" />
+          <Box height="926px" borderBottom="1px solid #C7C7C7">
+            <Input
+              onChange={titleChangeHandler}
+              borderColor="transparent"
+              placeholder="제목을 입력하세요."
+              _placeholder={{ color: "#C7C7C7" }}
+              fontSize="32px"
+              variant="unstyled"
+            />
+            <Box paddingTop="36px" />
+            <Flex
+              height="48px"
+              borderRadius="12px"
+              border="1px solid #C7C7C7"
+              alignItems="center"
+              paddingLeft="12px"
+            >
+              <FileSelectButton
+                onClick={selectFileHandler}
+                accept="application/x-sh"
+                onFileChange={fileChangeHandler}
+                selectedFile={selectedFile}
+              />
+              <Flex paddingLeft="15px" />
+              <Text
+                fontSize="18px"
+                color="gray"
+                _hover={selectedFile ? { color: "#FF7070" } : {}}
+                onClick={() => setSelectedFile(null)}
+              >
+                {selectedFile ? selectedFile.name : "선택된 파일이 없습니다."}
+              </Text>
+            </Flex>
+            <Box paddingTop="40px" />
+            <Textarea
+              height="700px"
+              placeholder="본문 내용을 입력해 주세요."
+              borderColor="transparent"
+              onChange={detailChangeHandler}
+              fontSize="22px"
+              variant="unstyled"
+              sx={{
+                "&::-webkit-scrollbar-thumb": {
+                  backgroundColor: "transparent",
+                },
+              }}
+            ></Textarea>
+          </Box>
+          <Box paddingTop="15px" />
+          <Box display="flex" justifyContent="flex-end">
+            <Button
+              type="submit"
+              borderRadius="20px"
+              border="1px solid #8E8E8E"
+              backgroundColor="transparent"
+              color="#8E8E8E"
+              _hover={{
+                border: "1.5px solid var(--Main-Orange, #FF9548)",
+                background: "var(--Light-Orange, #FFF0E5)",
+                color: "#FF9548",
+              }}
+            >
+              등록
+            </Button>
+          </Box>
+        </form>
+      </Box>
+    </BackGround>
   );
 };
 
---
+++ src/component/Util/Button/FileSelectButton.jsx
@@ -0,0 +1,22 @@
+import { Flex, Icon, Text } from "@chakra-ui/react";
+import { ReactComponent as PlusIcon } from "../../../asset/plus.svg";
+
+const FileSelectButton = ({ onClick, accept, onFileChange, selectedFile }) => {
+  return (
+    <Flex onClick={onClick} height="100%" alignItems="center" cursor="pointer">
+      <input
+        type="file"
+        id="fileInput"
+        onChange={onFileChange}
+        accept={accept}
+        style={{ display: "none" }}
+      />
+      <Icon as={PlusIcon} boxSize={6} />
+      <Text fontSize="18px" color="#FF9548" variant="ghost" fontWeight="700">
+        파일 선택
+      </Text>
+    </Flex>
+  );
+};
+
+export default FileSelectButton;
