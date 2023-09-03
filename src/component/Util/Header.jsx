c8dd94cc495b64f59290291218101e690152f905
Jincheol Park <67998022+Clearsu@users.noreply.github.com>
2023-09-03 23:50:35 +0900

    feat: header elements navigate and cancle writhe modal (#240)
    Co-authored-by: jiyun <70049994+tomatozil@users.noreply.github.com>
    Co-authored-by: jiyun <tomatoziyun@gmail.com>

--- src/component/Util/Header.jsx
+++ src/component/Util/Header.jsx
@@ -1,7 +1,29 @@
-import "./Header.css";
-import { Flex, Text } from "@chakra-ui/react";
+import { Flex, Text, Box } from "@chakra-ui/react";
+import Logo from "../Main/Logo";
+import { useNavigate } from "react-router-dom";
 
-const Header = (props) => {
+const Header = ({
+  pageTitle,
+  rightButton,
+  allowHomeNavigate,
+  allowBoardNavigate,
+  boardRoute,
+}) => {
+  const navigate = useNavigate();
+
+  const handleTitleClick = () => {
+    if (allowBoardNavigate) {
+      navigate(boardRoute);
+    }
+  };
+
+  const customStyles = allowBoardNavigate
+    ? {
+        cursor: "pointer",
+        onClick: handleTitleClick,
+      }
+    : {};
+
   return (
     <Flex
       height="114px"
@@ -9,10 +31,19 @@ const Header = (props) => {
       borderBottom="1px solid #C7C7C7"
       justifyContent="space-between"
     >
-      <Text fontSize="35px" fontWeight="700" marginLeft="32px" marginTop="45px">
-        {props.pageTitle}
-      </Text>
-      {props.rightButton}
+      <Flex
+        height="41px"
+        alignItems="center"
+        paddingLeft="32px"
+        marginTop="45px"
+      >
+        <Logo allowHomeNavigate={allowHomeNavigate} />
+        <Box paddingLeft="10px" />
+        <Text fontSize="30px" fontWeight="700" margin={0} {...customStyles}>
+          {pageTitle}
+        </Text>
+      </Flex>
+      {rightButton}
     </Flex>
   );
 };
