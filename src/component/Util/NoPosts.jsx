50108101e4567681dcbe88696f282c9118a9c2cf
Clearsu <clearwater1219@gmail.com>
2023-09-04 12:57:08 +0900

    feat: "no posts" message when no search result

---
+++ src/component/Util/NoPosts.jsx
@@ -0,0 +1,11 @@
+import { Text } from "@chakra-ui/react";
+
+const NoPosts = () => {
+  return (
+    <Text alignSelf="center" marginTop="100px" fontSize="20px" color="#8E8E8E">
+      게시글이 존재하지 않습니다.
+    </Text>
+  );
+};
+
+export default NoPosts;
