70287a3c2113fece3fdcd4dded72b6e13455912a
Jincheol Park <67998022+Clearsu@users.noreply.github.com>
2023-09-05 02:58:13 +0900

    feat: refactor, access control (#275)
    Co-authored-by: jiyun <70049994+tomatozil@users.noreply.github.com>
    Co-authored-by: jiyun <tomatoziyun@gmail.com>

--- src/util/isUrlValid.js
+++ src/util/isUrlValid.js
@@ -1,6 +1,6 @@
 const isUrlValid = (url) => {
   var pattern = new RegExp(
-    "^(https?:\\/\\/)?" +
+    "^(https:\\/\\/)" + // "https://"로 시작
       "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" +
       "((\\d{1,3}\\.){3}\\d{1,3}))" +
       "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" +
