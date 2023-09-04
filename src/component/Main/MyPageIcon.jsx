998e25552568e551a777c51f553c0291f240c96a
Jincheol Park <67998022+Clearsu@users.noreply.github.com>
2023-09-04 13:37:39 +0900

    feat: mypage icon hover (#260)
    Co-authored-by: jiyun <70049994+tomatozil@users.noreply.github.com>
    Co-authored-by: jiyun <tomatoziyun@gmail.com>

--- src/component/Main/MyPageIcon.jsx
+++ src/component/Main/MyPageIcon.jsx
@@ -1,11 +1,22 @@
 import { ReactComponent as Svg } from "../../asset/my-page-icon.svg";
 import { Link } from "react-router-dom";
-import { Icon } from "@chakra-ui/react";
+import { Icon, Flex } from "@chakra-ui/react";
 
 const MyPageIcon = ({ loginStateValue }) => {
   return (
     <Link to="/my-page">
-      {loginStateValue && <Icon as={Svg} w={6} h={6} display="inline-flex" />}
+      {loginStateValue && (
+        <Flex
+          width="33px"
+          height="33px"
+          borderRadius="8px"
+          _hover={{ background: "#F5F5F5" }}
+          align="center"
+          justify="center"
+        >
+          <Icon as={Svg} w={6} h={6} display="inline-flex" />
+        </Flex>
+      )}
     </Link>
   );
 };
