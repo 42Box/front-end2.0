f43adcacb189a7a0a729a4e22da2b24141a346db
Jincheol Park <67998022+Clearsu@users.noreply.github.com>
2023-09-05 19:02:02 +0900

    fix: logout api (#296)
    Co-authored-by: jiyun <70049994+tomatozil@users.noreply.github.com>
    Co-authored-by: jiyun <tomatoziyun@gmail.com>

--- src/component/Main/MainTitle.jsx
+++ src/component/Main/MainTitle.jsx
@@ -2,7 +2,12 @@ import { Text } from "@chakra-ui/react";
 
 const MainTitle = () => {
   return (
-    <Text fontSize="30px" fontWeight="700" display="inline-flex">
+    <Text
+      fontSize="30px"
+      fontWeight="700"
+      display="inline-flex"
+      paddingTop="10px"
+    >
       42Box
     </Text>
   );
