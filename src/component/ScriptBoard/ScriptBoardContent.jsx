d6627c206ebc182a66292d53589a911915109981
Clearsu <clearwater1219@gmail.com>
2023-09-06 04:56:38 +0900

    fix: wrong navigate

--- src/component/ScriptBoard/ScriptBoardContent.jsx
+++ src/component/ScriptBoard/ScriptBoardContent.jsx
@@ -148,7 +148,7 @@ const ScriptBoardContent = () => {
         pageTitle="스크립트"
         allowHomeNavigate={true}
         allowBoardNavigate={true}
-        boardRoute={"/service/board"}
+        boardRoute={"/script/board"}
       />
       <Flex
         flexDirection="column"
