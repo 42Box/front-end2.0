79c857a3906d6f2ae064dcc2c36543e27776853e
Clearsu <clearwater1219@gmail.com>
2023-09-05 23:13:45 +0900

    feat: fox runs

--- src/component/Main/IconBoardPreview.jsx
+++ src/component/Main/IconBoardPreview.jsx
@@ -1,4 +1,5 @@
 import { Box, Flex, Image } from "@chakra-ui/react";
+import { useState } from "react"; // useState 추가
 import BoardPreviewTitle from "./MainPreviewTitle";
 import apiCall from "../../util/apiCall";
 import box42 from "../../asset/42box.gif";
@@ -11,7 +12,9 @@ import foxTail from "../../asset/foxtail.gif";
 import runningFox from "../../asset/running-fox.gif";
 
 const IconBoardPreview = () => {
-  const gifs = [runningFox, foxTail, box42, flip42, gun, gon, gam, lee];
+  const gifs = [foxTail, box42, flip42, gun, gon, gam, lee];
+
+  const [isRunningFoxHovered, setIsRunningFoxHovered] = useState(false); // 호버 상태 저장
 
   const onClickHandler = async (icon) => {
     try {
@@ -28,17 +31,35 @@ const IconBoardPreview = () => {
     <>
       <Box paddingTop="40px" />
       <BoardPreviewTitle title="아이콘 변경" to="/icon/board" />
-      <Flex justifyContent="space-between" marginTop="20px">
+      <Flex justifyContent="space-between" marginTop="20px" height="40px">
+        <Image
+          src={runningFox}
+          onClick={() => onClickHandler(runningFox)}
+          cursor="pointer"
+          _hover={{
+            transform: "translateX(650px)",
+            transition: "transform 5s",
+          }}
+          onMouseEnter={() => setIsRunningFoxHovered(true)} // 호버 인 상태 변경
+          onMouseLeave={() => setIsRunningFoxHovered(false)} // 호버 아웃 상태 변경
+          style={{ zIndex: isRunningFoxHovered ? "2" : "auto" }} // 스택 순서 설정
+        />
         {gifs.map((gif, index) => {
           return (
             <Image
               src={gif}
               key={index}
-              width="45px"
-              height="45px"
               onClick={() => onClickHandler(gif)}
               cursor="pointer"
-              _hover={{ transform: "scale(1.5)", transition: "transform 0.3s" }}
+              style={{
+                filter: isRunningFoxHovered ? "blur(10px)" : "none", // 블러 효과 조건부 설정
+                transition: "filter 0.3s", // 필터 애니메이션 지속 시간
+                zIndex: isRunningFoxHovered ? "1" : "auto", // 스택 순서 설정
+              }}
+              _hover={{
+                transform: "scale(1.5)",
+                transition: "transform 0.3s",
+              }}
             />
           );
         })}
