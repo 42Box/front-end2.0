8c27dd535b112faff7f420a1f41a807634415f4a
Jincheol Park <67998022+Clearsu@users.noreply.github.com>
2023-09-05 14:35:43 +0900

    chore: 주석처리 (#287)
    Co-authored-by: jiyun <70049994+tomatozil@users.noreply.github.com>
    Co-authored-by: jiyun <tomatoziyun@gmail.com>

--- src/component/ServiceBoard/ServiceBoardContent.jsx
+++ src/component/ServiceBoard/ServiceBoardContent.jsx
@@ -121,7 +121,7 @@ const ServiceBoardContent = () => {
             open={errorAlert.alertData.isOpen}
             close={() => {
               errorAlert.closeAlert();
-              navigate(`/service/content/${postId}`);
+              // navigate(`/service/content/${postId}`);
             }}
             header={errorAlert.alertData.title}
           >
@@ -133,7 +133,7 @@ const ServiceBoardContent = () => {
             open={successAlert.alertData.isOpen}
             close={() => {
               successAlert.closeAlert();
-              navigate(`/script/content/${postId}`);
+              // navigate(`/script/content/${postId}`);
             }}
             header={successAlert.alertData.title}
           >
