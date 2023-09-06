326151e16a6c1eee6d3e6db9a9603d9ba093b656
Clearsu <clearwater1219@gmail.com>
2023-09-07 03:49:28 +0900

    test: change dummy code

--- src/dummyComments.js
+++ src/dummyComments.js
@@ -2,25 +2,25 @@ const dummyComments = [
   {
     id: Math.random(),
     date: new Date(2023, 11, 21),
-    boardType: "스크립트",
-    title: "이거 이렇게 하는거 맞나요?",
-    content: "엥 이건 좀 아닌듯 다시 공부하십쇼",
+    boardType: "서비스 등록",
+    title: "42Box 팀입니다!!",
+    content: "와 이거 신박하네요 추천 드립니다",
   },
   {
     id: Math.random(),
     writerName: "jincpark",
     date: new Date(2023, 11, 21),
     boardType: "스크립트",
-    title: "이거 이렇게 하는거 맞나요?",
-    content: "엥 이건 좀 아닌듯 다시 공부하십쇼",
+    title: "맥 환경 세팅 스크립트입니다",
+    content: "단축키 한 번에 쉽게 세팅할 수 있다니 진짜 너무 좋네요",
   },
   {
     id: Math.random(),
     writerName: "jincpark",
     date: new Date(2023, 11, 21),
     boardType: "스크립트",
-    title: "이거 이렇게 하는거 맞나요?",
-    content: "엥 이건 좀 아닌듯 다시 공부하십쇼",
+    title: "node 설치 스크립트입니다.",
+    content: "설치가 잘 안 되는데 확인 부탁드립니다.",
   },
 ];
 
--- src/dummyPosts.js
+++ src/dummyPosts.js
@@ -3,7 +3,7 @@ const dummyPosts = [
     writerProfileImagePath:
       "https://cdn.intra.42.fr/users/73139f797eb48e61e4ba1d437171c56d/sechung.jpg",
     boardId: Math.random(),
-    title: "Hello World",
+    title: "42Box 팀입니다.",
     writerName: "jincpark",
     content: "sample content",
     commentCount: 10,
@@ -14,23 +14,23 @@ const dummyPosts = [
     writerProfileImagePath:
       "https://cdn.intra.42.fr/users/73139f797eb48e61e4ba1d437171c56d/sechung.jpg",
     boardId: Math.random(),
-    title: "Hello World",
+    title: "webserv 하실 때 여기 참고하시면 진짜 좋아요",
     writerName: "jincpark",
     content: "sample content",
     commentCount: 10,
     likeCount: 10,
-    regDate: new Date(2023, 8, 11),
+    regDate: new Date(2022, 10, 1),
   },
   {
     writerProfileImagePath:
       "https://cdn.intra.42.fr/users/73139f797eb48e61e4ba1d437171c56d/sechung.jpg",
     boardId: Math.random(),
-    title: "Hello World",
+    title: "서비스 추천 드립니다!!!",
     writerName: "jincpark",
     content: "sample content",
     commentCount: 10,
     likeCount: 10,
-    regDate: new Date(2023, 8, 11),
+    regDate: new Date(2022, 5, 21),
   },
 ];
 
